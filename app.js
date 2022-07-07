d85f5e267fb5b08e9aaef688d50ddb3620520f95
Yordan Dimitrov <79158804+yordanfilipov@users.noreply.github.com>
2022-07-07 15:40:02 +0300

    Update app.js

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb+srv://admin-suraj:test123@cluster0.kt331.mongodb.net/todolistDB", {useNewUrlParser: true});
+mongoose.connect("mongodb+srv://admin-yordan:test123@cluster0.kt331.mongodb.net/todolistDB", {useNewUrlParser: true});
 
 const itemsSchema = {
   name: String
